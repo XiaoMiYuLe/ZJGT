d226d5ae3cb8eee4791cb1b73dece1df7f615cb9
梁海冬 <lianghaidong@chinazjgt.com>
2017-10-18 16:14:03 +0800

    提交备份（未安装webpack，只安装thinkjs依赖的webpack） npm install think-webpack-hot-middleware think-webpack-dev-middleware --save-dev

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+//var webpack = require('webpack');
+//module.exports = {
+//    entry: {
+//        main: [
+//            'src/index.js',
+//            'webpack-hot-middleware/client?reload=true'
+//        ]
+//    },
+//    plugin: [
+//       new webpack.HotModuleReplacementPlugin()
+//    ]
+//};
