4f5f7679748d5c1852a4d112584cb74876be7f86
梁海冬 <lianghaidong@chinazjgt.com>
2017-11-14 15:19:54 +0800

    修改备注

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
             }
         }),
         new HtmlWebpackPlugin({
-            title: "This is the result", //生成的HTML模板的title，如果模板中有设置title的名字，则会忽略这里的设置
+            title: "This is the result", //生成的HTML模板的title，如果模板中有设置title的名字，则会忽略这里的设置 html页面这样写<%= htmlWebpackPlugin.options.title %>
             filename: "../../../../view/header2.html", //生成的模板文件的名字
             template: "./view/header.html", //模板来源文件
             inject: 'head', //引入模块的注入位置；取值有true/false/body/head
@@ -35,9 +35,9 @@ module.exports = {
             hash: false, //是否生成hash添加在引入文件地址的末尾，类似于我们常用的时间戳
             cache: true, //是否需要缓存，如果填写true，则文件只有在改变时才会重新生成
             showErrors: true, //是否将错误信息写在页面里，默认true，出现错误信息则会包裹在一个pre标签内添加到页面上
-            chunks: "", //引入的模块，这里指定的是entry中设置多个js时，在这里指定引入的js，如果不设置则默认全部引入
-            chunksSortMode: "auto", //引入模块的排序方式
-            excludeChunks: "", //排除的模块
+            chunks: ['wp_header'], //引入的模块，这里指定的是entry中设置多个js时，在这里指定引入的js，如果不设置则默认全部引入
+            chunksSortMode: "auto", //这个选项决定了 script（引入的模块） 标签的引用顺序。默认有四个选项，'none', 'auto', 'dependency', '{function}'。
+            excludeChunks: "", //排除的模块,和chunks相反
             xhtml: false //生成的模板文档中标签是否自动关闭，针对xhtml的语法，会要求标签都关闭，默认false
         })
     ]
